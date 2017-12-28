51b5dda4ef09230c952d991b53b3e69fce470631
Nabywaniec <Nabywaniec@users.noreply.github.com>
2017-12-28 16:40:10 +0100

    Create ActiveObject.java

---
+++ src/ActiveObject.java
@@ -0,0 +1,25 @@
+/**
+ * Created by jacek on 29.11.17.
+ */
+public class ActiveObject {
+
+    private Scheduler scheduler;
+    private Proxy proxy;
+    private FileBuffer buffer;
+
+    public ActiveObject(int bufferSize){
+        this.scheduler = new Scheduler();
+        this.buffer = new FileBuffer(bufferSize);
+        this.proxy = new Servant(buffer, scheduler);
+        scheduler.start();
+    }
+
+    public Proxy getProxy(){
+        return this.proxy;
+    }
+
+    public void start(){
+        this.scheduler.start();
+    }
+
+}
