5d3b87f2eada370cd38577bc2420d4eb18c471a4
Nabywaniec <Nabywaniec@users.noreply.github.com>
2017-12-28 16:42:31 +0100

    Create File.java

---
+++ src/File.java
@@ -0,0 +1,27 @@
+/**
+ * Created by jacek on 28.12.17.
+ */
+public class File {
+
+    private String name;
+    private String content;
+
+    public File(String name, String content){
+        this.name = name;
+        this.content = content;
+    }
+
+    private void setContent(String content){
+        this.content = content;
+    }
+
+    public String getName(){
+        return  this.name;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+
+}
