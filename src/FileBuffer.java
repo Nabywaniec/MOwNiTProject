72d9ce5dabc08182d8c0953ca823d4738f95e11a
Nabywaniec <Nabywaniec@users.noreply.github.com>
2017-12-28 16:42:54 +0100

    Create FileBuffer.java

---
+++ src/FileBuffer.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+
+/**
+ * Created by jacek on 28.12.17.
+ */
+public class FileBuffer {
+
+    private LinkedList<File> files = new LinkedList<>();
+    private int buffersize;
+
+    public FileBuffer(int buffersize){
+        this.buffersize = buffersize;
+    }
+
+    public void add(File file){
+        this.files.add(file);
+    }
+
+    public File findFile(String name){
+        for(File f : files){
+            if(f.getName().equals(name)){
+                return f;
+            }
+        }
+        return null;
+    }
+
+
+    public File removeFile(){
+            if(! this.isEmpty()){
+                File f = this.files.get(0);
+                this.files.remove(0);
+                return f;
+        }
+        System.out.println("Nie ma takiego pliku");
+        return null;
+    }
+
+    public boolean isFull(){
+        return this.files.size() == buffersize;
+    }
+
+    public boolean isEmpty(){return this.files.size() == 0;}
+
+}
