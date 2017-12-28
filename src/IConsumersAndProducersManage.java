0d2876ca123d684e4a88426bec696239ab7989ce
Nabywaniec <Nabywaniec@users.noreply.github.com>
2017-12-28 16:43:52 +0100

    Create IConsumersAndProducersManage.java

---
+++ src/IConsumersAndProducersManage.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jacek on 13.12.17.
+ */
+public interface IConsumersAndProducersManage {
+
+    void startProducer();
+    void deleteProducer(int id);
+    void startConsumer();
+    void deleteConsumer(int id);
+
+}
