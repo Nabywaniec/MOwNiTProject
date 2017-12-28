424dab7b0823b9c13f8a05e00075c87911706415
Nabywaniec <Nabywaniec@users.noreply.github.com>
2017-12-28 16:45:54 +0100

    Create Servant.java

---
+++ src/Servant.java
@@ -0,0 +1,26 @@
+/**
+ * Created by jacek on 29.11.17.
+ */
+public class Servant implements Proxy {
+
+    private FileBuffer buffer;
+    private Scheduler scheduler;
+
+    public Servant(FileBuffer buffer, Scheduler scheduler){
+        this.buffer = buffer;
+        this.scheduler = scheduler;
+    }
+
+    public void add(File file){
+        this.scheduler.add(new AddMethod(this.buffer,file));
+
+    }
+
+    public Future remove(){
+        Future future = new Future();
+        this.scheduler.add(new DeleteMethod(buffer, future));
+        return future;
+    }
+
+
+}
